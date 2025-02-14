7a39d629935d6ed69cb1fe312b544b2ec138a2eb
Joshua <187735522+joshuadrom03@users.noreply.github.com>
2025-02-14 11:39:45 +0200

    added the gameplay function,score function and highscore function

--- memory-card/src/Memory.jsx
+++ memory-card/src/Memory.jsx
@@ -1,20 +1,46 @@
 
-import { useState } from "react"
+import {useState } from "react"
 
     export function GenerateCards(){
 
         const [cards, setCards] = useState(["funny-alien", "pondering-alien", "sleeping-alien", "dancing-alien", "spinning-alien", "drinking-alien", "suspect-alien", "high-five-alien", "wave-alien"]);
-        const [className, setClassName] = useState("card")
+        const [selectedCards, setSelectedCards] = useState([])
         const [score, setScore] = useState(0)
         const [highScore, setHighScore] = useState(0)
 
         const randomizeCards = () => {
             const random = [...cards].sort(() => Math.random() -0.5)
             setCards(random);
+
         }
 
-        
+        const gamePlay = (id) => {
 
+            randomizeCards();
+            id = (event.target.id)
+            setSelectedCards((prevIds) => [...prevIds, id])
+
+            if(selectedCards.includes(id)){
+                setScore(0)
+                setSelectedCards([])
+            } else{
+                displayScore();
+                displayHighScore();
+            }
+            
+        }
+
+        const displayScore = () => {
+            setScore((prevScore) => prevScore + 1)
+        }
+
+        const displayHighScore = () => {
+            if(score >= highScore){
+                setHighScore((prevHighScore) => prevHighScore + 1);
+            }  else{
+                setHighScore(highScore);
+            }
+        }
 
         return(
         
@@ -32,9 +58,10 @@ import { useState } from "react"
                 <div className="cards-container">
                 {cards.map((card, index) => (
                 <div key={index} 
-                className={className} 
-                id= {card}
-                onClick={() => randomizeCards()}>
+                className="card"
+                id={card}
+                onClick={() => gamePlay(card) }
+                >
                     <div className="card-text">{card}</div>
                 </div>
                 ))}
